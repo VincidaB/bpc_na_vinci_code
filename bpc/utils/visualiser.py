9d9d747929530c870bf397bde8a25f1b2bc448c7
Vincent Belpois <vincent.belpois@gmail.com>
2025-02-17 21:54:07 +0100

    Added support for multiple point clouds

--- bpc/utils/visualiser.py
+++ bpc/utils/visualiser.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 import open3d.visualization.gui as gui
 import open3d.t.geometry as tgeometry
+import random
 
 dataset_path = "/media/vincent/more/bpc_teamname/datasets/ipd"
 split = "test"
@@ -58,15 +59,18 @@ depth_images = [
 camera_intrinsics = [
     get_camera_intrinsic(split, scene_id, img_id, camera) for camera in cameras
 ]
+camera_extrinsics = [
+    get_camera_extrinsics(split, scene_id, img_id, camera) for camera in cameras
+]
 
 # create a point cloud from depth images, for now just from the first camera
-depth_image = depth_images[0]
-rgb_image = rgb_images[0]
+#depth_image = depth_images[0]
+#rgb_image = rgb_images[0]
 
-RESIZE_FACTOR = 1.0
+RESIZE_FACTOR = 0.5
 
-depth_image = cv2.resize(depth_image, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
-rgb_image = cv2.resize(rgb_image, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
+#depth_image = cv2.resize(depth_image, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
+#rgb_image = cv2.resize(rgb_image, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
 
 # camera intrinsics
 # create a point cloud from depth image
@@ -77,42 +81,82 @@ cx = cam_k[2] * RESIZE_FACTOR
 cy = cam_k[5] * RESIZE_FACTOR
 
 print(fx, fy, cx, cy)
+print("cam_k")
+print(cam_k)
 
-factor = 10000.0
+#factor = 10000.0
+#depth = depth_image.astype(float) / factor
+#rows, cols = depth.shape
+#c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
+#Z = depth
+#X = (c - cx) * Z / fx
+#Y = (r - cy) * Z / fy
+#pcd = o3d.geometry.PointCloud()
+#pcd.points = o3d.utility.Vector3dVector(np.dstack((X, Y, Z)).reshape(-1, 3))
+#pcd.colors = o3d.utility.Vector3dVector(rgb_image.reshape(-1, 3) / 255.0)
+#pcd_t = tgeometry.PointCloud.from_legacy(pcd)
 
-depth = depth_image.astype(float) / factor
-rows, cols = depth.shape
-c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
-Z = depth
-X = (c - cx) * Z / fx
-Y = (r - cy) * Z / fy
 
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(np.dstack((X, Y, Z)).reshape(-1, 3))
-pcd.colors = o3d.utility.Vector3dVector(rgb_image.reshape(-1, 3) / 255.0)
+class camera_pov:
+    def __init__(self, camera_intrinsics, camera_extrinsics, rgb_image, depth_image, resize_factor=1.0):
+        self.camera_intrinsics = camera_intrinsics
+        self.camera_extrinsics = camera_extrinsics
+        self.rgb_image = rgb_image
+        self.depth_image = depth_image
+        
+        self.point_cloud = None
+        self.false_color = False
 
+        if resize_factor != 1.0:
+            self.depth_image = cv2.resize(depth_image, (0, 0), fx=resize_factor, fy=resize_factor)
+            self.rgb_image = cv2.resize(rgb_image, (0, 0), fx=resize_factor, fy=resize_factor)
+            # TODO : verify we are only scaling the camera intrinsics and not the last value which is 1.0
+            print(self.camera_intrinsics)
+            self.camera_intrinsics[:-1] = [i * resize_factor for i in camera_intrinsics[:-1]]
+            print("Resized camera intrinsics")
+            print(self.camera_intrinsics)
 
-pcd_t = tgeometry.PointCloud.from_legacy(pcd)
+    def get_point_cloud(self) -> tgeometry.PointCloud:
+        if self.point_cloud != None:
+            return self.point_cloud
+        
+
+        depth_factor = 10000.0
+
+        depth = self.depth_image.astype(float) / depth_factor
+        rows, cols = depth.shape
+        c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
+        Z = depth
+        X = (c - cx) * Z / fx
+        Y = (r - cy) * Z / fy
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(np.dstack((X, Y, Z)).reshape(-1, 3))
+        pcd.colors = o3d.utility.Vector3dVector(self.rgb_image.reshape(-1, 3) / 255.0)
+        self.point_cloud = tgeometry.PointCloud.from_legacy(pcd)
+        return self.point_cloud
 
 
 class PointCloudVisualizer:
     def __init__(self):
         self.coloured = False
-        self.lit = True
+        self.lit = False
+
+        self.pov_cams = {}
 
         gui.Application.instance.initialize()
-        self.window = gui.Application.instance.create_window("Open3D", 1920, 1080)
+        self.window = gui.Application.instance.create_window("Point cloud pov viewer", 1920, 1080)
 
         self.scene = gui.SceneWidget()
         self.scene.scene = o3d.visualization.rendering.Open3DScene(self.window.renderer)
 
         self.scene.scene.set_background([1, 1, 1, 1])
         self.scene.scene.scene.set_sun_light(
-            [-1, -1, -1], [1, 1, 1], 100000  # direction  # color
-        )  # intensity
+            [-1, -1, -1], [1, 1, 1], 100000  
+        ) # direction # color # intensity
         self.scene.scene.scene.enable_sun_light(True)
-        bbox = o3d.geometry.AxisAlignedBoundingBox([-10, -10, -10], [10, 10, 10])
+        bbox = o3d.geometry.AxisAlignedBoundingBox([-1, -1, -1], [1, 1, 1])
         self.scene.setup_camera(60, bbox, [0, 0, 0])
+        self.scene.scene.camera.look_at([0, 0, 0], [0, 0, -1], [0, -1, 0])
         self.window.add_child(self.scene)
 
         em = self.window.theme.font_size
@@ -125,17 +169,17 @@ class PointCloudVisualizer:
             "View controls", 0.25 * em, gui.Margins(em, 0, 0, 0)
         )
 
-        self._color_clouds_button = gui.Button("Toggle cloud RGB coloring")
+        self._color_clouds_button = gui.Button("Toggle false colors")
         self._color_clouds_button.horizontal_padding_em = 0.5
         self._color_clouds_button.vertical_padding_em = 0
-        self._color_clouds_button.set_on_clicked(self.change_point_clouds_color)
+        self._color_clouds_button.set_on_clicked(lambda: self.change_point_clouds_color())
 
         self._lit_unlit_button = gui.Button("Lit / Unlit")
         self._lit_unlit_button.horizontal_padding_em = 0.5
         self._lit_unlit_button.vertical_padding_em = 0
         self._lit_unlit_button.set_on_clicked(self.set_lit_unlit)
 
-        view_ctrls.add_child(gui.Label("Mouse controls"))
+        view_ctrls.add_child(gui.Label("Controls"))
         h = gui.Horiz(0.25 * em)  # row 1
         h.add_stretch()
         h.add_child(self._color_clouds_button)
@@ -154,47 +198,66 @@ class PointCloudVisualizer:
         app_menu = gui.Menu()
         app_menu.add_item("quit", 1)
         menu = gui.Menu()
-        menu.add_menu("app", app_menu)
+        menu.add_menu("Nothing to see here", app_menu)
 
         gui.Application.instance.menubar = menu
 
         material = o3d.visualization.rendering.MaterialRecord()
-        material.shader = "defaultLit"
+        material.shader = "defaultUnlit"
 
-        self.scene.scene.add_geometry("point_cloud", pcd_t, material)
+        # add a cube at zero zero zero
+        cube = o3d.geometry.TriangleMesh.create_box(0.05, 0.05, 0.05)
+        cube_t = tgeometry.TriangleMesh.from_legacy(cube)
+        cube_t.translate([0, 0, 0])
+        material.base_color = [1, 0, 0, 1]
+        self.scene.scene.add_geometry("cube", cube_t, material)
 
+    def add_point_cloud(self, pov_cam : camera_pov, name="point_cloud"):
+        if self.scene.scene.has_geometry(name):
+            name = name + "_1"
+        print(f"Adding point cloud with name '{name}'")
+        material = o3d.visualization.rendering.MaterialRecord()
+        material.shader = "defaultUnlit"
+        self.scene.scene.add_geometry(name, pov_cam.get_point_cloud(), material)
+        print(f"Added point cloud with name '{name}'")
+        # adding the camera pov to the dictionary with it's name as the key
+        self.pov_cams[name] = pov_cam
+
+    # TODO : change this from using rgb_image to using a `point_of_view` object
+    # TODO : The `point_of_view` object needs to be created and will hold the camera intrinsics and extrinsics, 
+    # TODO :    the rgb_image and the depth_image, the state of the coloring
     def change_point_clouds_color(self):
-        if self.coloured:
-            print("Changing back to the original color")
-            self.coloured = False
-            pcd_t.point["colors"] = o3d.core.Tensor(
-                rgb_image.reshape(-1, 3) / 255.0, dtype=o3d.core.Dtype.Float32
-            )
-        else:
-            self.coloured = True
-            pcd_t.point["colors"] = o3d.core.Tensor(
-                np.ones_like(rgb_image).reshape(-1, 3) * [0, 1, 0],
-                dtype=o3d.core.Dtype.Float32,
-            )
-        if self.scene.scene.has_geometry("point_cloud"):
-            self.scene.scene.scene.update_geometry(
-                "point_cloud",
-                pcd_t,
-                o3d.visualization.rendering.Scene.UPDATE_COLORS_FLAG,
-            )
-        else:
-            print("No geometry found")
+        for name, pov_cam in self.pov_cams.items():
+            
+            if pov_cam.false_color:
+                print("Changing back to the original color")
+                pov_cam.false_color = False
+                pov_cam.point_cloud.point["colors"] = o3d.core.Tensor(
+                    pov_cam.rgb_image.reshape(-1, 3) / 255.0, dtype=o3d.core.Dtype.Float32
+                )
+            else:
+                # use the name of the camera to determine the color
+                np.random.seed(sum(ord(char) for char in name))
+                color = np.random.rand(3)
+                pov_cam.false_color = True
+                pov_cam.point_cloud.point["colors"] = o3d.core.Tensor(
+                    np.ones_like(pov_cam.rgb_image).reshape(-1, 3) * color,
+                    dtype=o3d.core.Dtype.Float32,
+                )
+            if self.scene.scene.has_geometry(name):
+                self.scene.scene.scene.update_geometry(
+                    name,
+                    pov_cam.point_cloud,
+                    o3d.visualization.rendering.Scene.UPDATE_COLORS_FLAG,
+                )
+            else:
+                print(f"No geometry with the name '{name}' found")
 
     def set_lit_unlit(self):
         material = o3d.visualization.rendering.MaterialRecord()
-        if self.lit:
-            material.shader = "defaultUnlit"
-            self.scene.scene.update_material(material)
-            self.lit = False
-        else:
-            material.shader = "defaultLit"
-            self.scene.scene.update_material(material)
-            self.lit = True
+        self.lit = not self.lit
+        material.shader = "defaultLit" if self.lit else "defaultUnlit"
+        self.scene.scene.update_material(material)
 
     def _on_layout(self, layout_context):
         r = self.window.content_rect
@@ -214,4 +277,10 @@ class PointCloudVisualizer:
 
 if __name__ == "__main__":
     visualizer = PointCloudVisualizer()
+    pov_cam1 = camera_pov(camera_intrinsics[0], camera_extrinsics[0], rgb_images[0], depth_images[0], resize_factor=RESIZE_FACTOR)
+    pov_cam2 = camera_pov(camera_intrinsics[1], camera_extrinsics[1], rgb_images[1], depth_images[1], resize_factor=RESIZE_FACTOR)
+    pov_cam3 = camera_pov(camera_intrinsics[2], camera_extrinsics[2], rgb_images[2], depth_images[2], resize_factor=RESIZE_FACTOR)
+    visualizer.add_point_cloud(pov_cam1, "cam_1")
+    visualizer.add_point_cloud(pov_cam2, "cam_2")
+    visualizer.add_point_cloud(pov_cam3, "cam_3")
     visualizer.run()
