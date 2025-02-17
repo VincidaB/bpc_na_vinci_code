8b5048b1f5087e9c6ab0e75732a100f0536ba14c
Vincent Belpois <vincent.belpois@gmail.com>
2025-02-17 13:17:58 +0100

    Initial util for point cloud visualization working

---
+++ bpc/utils/visualiser.py
@@ -0,0 +1,75 @@
+import open3d as o3d
+import cv2
+import numpy as np
+
+dataset_path = "/media/vincent/more/bpc_teamname/datasets/ipd"
+split = "test"
+scene_id = 0
+img_id = 0
+
+modalities = ["rgb", "depth"]
+cameras = ["cam1", "cam2", "cam3"]
+
+
+def image_path(split, scene_id, image_id, modality, camera):
+    if split == "train_pbr":
+        ext = "jpg"
+    else:
+        ext = "png"
+    return f"{dataset_path}/{split}/{scene_id:06d}/{modality}_{camera}/{image_id:06d}.{ext}"
+
+
+rgb_images = [
+    cv2.imread(image_path(split, scene_id, img_id, "rgb", camera)) for camera in cameras
+]
+depth_images = [
+    cv2.imread(
+        image_path(split, scene_id, img_id, "depth", camera), cv2.IMREAD_UNCHANGED
+    )
+    for camera in cameras
+]
+
+# create a point cloud from depth images, for now just from the first camera
+
+depth_image = depth_images[0]
+rgb_image = rgb_images[0]
+
+RESIZE_FACTOR = 1.0
+
+depth_image = cv2.resize(depth_image, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
+rgb_image = cv2.resize(rgb_image, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
+
+# camera intrinsics
+# create a point cloud from depth image
+cam_k = [
+    3981.985991142684,
+    0.0,
+    1954.1872863769531,
+    0.0,
+    3981.985991142684,
+    1103.6978149414062,
+    0.0,
+    0.0,
+    1.0,
+]
+
+fx = cam_k[0] * RESIZE_FACTOR
+fy = cam_k[4] * RESIZE_FACTOR
+cx = cam_k[2] * RESIZE_FACTOR
+cy = cam_k[5] * RESIZE_FACTOR
+
+print(fx, fy, cx, cy)
+
+factor = 10000.0
+
+depth = depth_image.astype(float) / factor
+rows, cols = depth.shape
+c, r = np.meshgrid(np.arange(cols), np.arange(rows), sparse=True)
+Z = depth
+X = (c - cx) * Z / fx
+Y = (r - cy) * Z / fy
+
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(np.dstack((X, Y, Z)).reshape(-1, 3))
+pcd.colors = o3d.utility.Vector3dVector(rgb_image.reshape(-1, 3) / 255.0)
+o3d.visualization.draw_geometries([pcd])
